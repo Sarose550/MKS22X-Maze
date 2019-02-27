01dbfde65d455292d5d12b87fb8cc7ac4f359a2c
Sarose550 <43526712+Sarose550@users.noreply.github.com>
2019-02-27 10:35:46 -0500

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -1,12 +1,16 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.*;
 import java.util.Scanner;
 public class Maze{
-  public static void main(String args[]) throws FileNotFoundException{
+
+	private char[][] maze;
+	private booelan animate;
+
+  	public Maze(String filename) throws FileNotFoundException{
         //instead of a try/catch, you can throw the FileNotFoundException.
         //This is generally bad behavior
  
-        File text = new File("Maze1.txt");
+        File text = new File(filename);
         // can be a path like: "/full/path/to/file.txt" or "../data/file.txt"
         
         //inf stands for the input file
@@ -17,7 +21,6 @@ public class Maze{
             String line = inf.nextLine();
             l = line.length();
             w++;
-            System.out.println(line);//hopefully you can do other things with the line
         }
         char[][] maze = new char[w][l];
         Scanner bob = new Scanner(text);
@@ -28,12 +31,6 @@ public class Maze{
                 maze[row][i] = line.charAt(i);
             }
             row++;
-        }
-        for(int i = 0; i < w; i++){
-            for(int j = 0; j < l; j++){
-                System.out.print(maze[i][j]);
-            }
-            System.out.println();
         }
     }   
 }
