d1149f3997d85e51ac36a94f449519e40eda0e86
Sarose550 <43526712+Sarose550@users.noreply.github.com>
2019-02-28 11:01:15 -0500

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -5,6 +5,10 @@ public class Maze{
 
 	private char[][] maze;
 	private booelan animate;
+	private int width;
+	private int length;
+	private int startx;
+	private int starty;
 
   	public Maze(String filename) throws FileNotFoundException{
         //instead of a try/catch, you can throw the FileNotFoundException.
@@ -22,15 +26,55 @@ public class Maze{
             l = line.length();
             w++;
         }
-        char[][] maze = new char[w][l];
+        width = w;
+        length = l;
+        maze = new char[w][l];
         Scanner bob = new Scanner(text);
         int row = 0;
         while(bob.hasNextLine()){
             String line = bob.nextLine();
             for(int i = 0; i < line.length(); i++){
                 maze[row][i] = line.charAt(i);
+                if(maze[row][i] == 'S'){
+                	startx = i;
+                	starty = row;
+                }
             }
             row++;
         }
-    }   
+    }
+
+    public String toString(){
+    	String result = "";
+    	for(int i = 0; i < width; i++){
+    		for(int j = 0; j < length; j++){
+    			result += maze[i][j];
+    		}
+    		result += "\n";
+    	}
+    	return result;
+    }
+
+    public void setAnimate(boolean b){
+        animate = b;
+    }
+
+    public int solve(){
+    	return this.solveH(startx, starty);
+    }
+
+    public int solveH(int x, int y){
+    	for(int i = -1; i <= 1; i += 2){
+    		for(int j = -1; j <= 1; j+=2){
+    			if(maze[x][y] == 'E') return 0;
+    			if(maze[x][y] == ' '){
+    				maze[x][y] = '@';
+    				int k = this.solveH(x + i, y + j);
+    				if(k != -1) return 1 + k;
+    				maze[x][y] = '.';
+    			}
+    		}
+    	}
+    	return -1;
+    }
 }
